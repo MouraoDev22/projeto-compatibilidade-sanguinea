adaac80cee955a0c58d6336fcc91ab2ab8745b64
Pedro <p3dr0sk0sk7160@gmail.com>
2025-10-01 20:14:59 -0300

    ajustes

---
+++ compatibilidade-sanguinea-v1.js
@@ -0,0 +1,102 @@
+// variáveis
+
+let nome;
+let idade;
+let tipoSanguineo;
+let fatorRh;
+
+let listaDeDoadores = [];
+let listaDeRecebedores = [];
+
+// funções
+
+function retornarNome () {
+    nome = prompt ('Qual é o seu nome completo?');
+}
+
+function retornarIdade () {
+    idade = Number (prompt ('Qual a sua idade?'));
+    while (isNaN (idade)) {
+        idade = Number (prompt ('Qual a sua idade?'));
+    }
+}
+
+function retornarTipoSanguineo () {
+    tipoSanguineo = prompt ('Qual é o seu tipo sanguíneo?').trim ().toUpperCase ();
+    
+    while (tipoSanguineo !== "A" && tipoSanguineo !== "B" && tipoSanguineo !== "AB" && tipoSanguineo !== "O") {
+        tipoSanguineo = prompt ('Qual é o seu tipo sanguíneo?').trim ().toUpperCase ();
+    }
+}
+
+function retornarFatorRh () {
+    fatorRh = prompt ('É positivo (+) ou negativo (-)?').trim ().toLowerCase ();
+
+    while (fatorRh !== "-" && fatorRh !== "+" && fatorRh !== "negativo" && fatorRh !== "positivo") {
+        fatorRh = prompt ('É positivo (+) ou negativo (-)?').trim ().toLowerCase ();
+    }
+}
+
+function verificarCompatibilidade () {
+    const rh = (fatorRh == 'positivo' || fatorRh == '+') ? '+' : '-';
+    fatorRh = rh;
+
+    // ----- Quem pode RECEBER meu sangue -----
+    
+    if (tipoSanguineo == 'O' && rh == '+') {
+        listaDeRecebedores.push ('A+', 'B+', 'AB+', 'O+');
+    } else if (tipoSanguineo == 'O' && rh == '-') {
+        listaDeRecebedores.push ('A+', 'B+', 'AB+', 'O+', 'A-', 'B-', 'AB-', 'O-');
+    } else if (tipoSanguineo == 'A' && rh == '+') {
+        listaDeRecebedores.push ('A+', 'AB+');
+    } else if (tipoSanguineo == 'A' && rh == '-') {
+        listaDeRecebedores.push ('A+', 'A-', 'AB+', 'AB-');
+    } else if (tipoSanguineo == 'B' && rh == '+') {
+        listaDeRecebedores.push ('B+', 'AB+');
+    } else if (tipoSanguineo == 'B' && rh == '-') {
+        listaDeRecebedores.push ('B+', 'B-', 'AB+', 'AB-');
+    } else if (tipoSanguineo == 'AB' && rh == '+') {
+        listaDeRecebedores.push ('AB+');
+    } else if (tipoSanguineo == 'AB' && rh == '-') {
+        listaDeRecebedores.push ('AB+', 'AB-');
+    }
+
+    // ----- De quem POSSO RECEBER -----
+    
+    if (tipoSanguineo == 'O' && rh == '+') {
+        listaDeDoadores.push ('O+', 'O-');
+    } else if (tipoSanguineo == 'O' && rh == '-') {
+        listaDeDoadores.push ('O-');
+    } else if (tipoSanguineo == 'A' && rh == '+') {
+        listaDeDoadores.push ('A+', 'A-', 'O+', 'O-');
+    } else if (tipoSanguineo == 'A' && rh == '-') {
+        listaDeDoadores.push ('A-', 'O-');
+    } else if (tipoSanguineo == 'B' && rh == '+') {
+        listaDeDoadores.push ('B+', 'B-', 'O+', 'O-');
+    } else if (tipoSanguineo == 'B' && rh == '-') {
+        listaDeDoadores.push ('B-', 'O-');
+    } else if (tipoSanguineo == 'AB' && rh == '+') {
+        listaDeDoadores.push ('A+', 'A-', 'O+', 'O-', 'B+', 'B-', 'AB+', 'AB-');
+    } else if (tipoSanguineo == 'AB' && rh == '-') {
+        listaDeDoadores.push ('A-', 'B-', 'AB-', 'O-');
+    }
+}
+
+// prompts
+
+retornarNome ();
+retornarIdade ();
+retornarTipoSanguineo ();
+retornarFatorRh ();
+verificarCompatibilidade ();
+
+// logs
+
+console.log ('');
+console.log ('Ficha de Compatibilidade Sanguínea');
+console.log (`Nome: ${nome}`);
+console.log (`Idade: ${idade}`);
+console.log (`Tipo Sanguíneo: ${tipoSanguineo}`);
+console.log (`Fator RH: ${fatorRh}`);
+console.log ('Pode receber de:', listaDeDoadores.join (', '));
+console.log ('Pode doar para:', listaDeRecebedores.join (', '));
